ccb8e3b3994632531fb397f532afbcc9c11dbcea
giuseppesciacca <sciaccagi@gmail.com>
2023-06-07 16:52:00 +0200

    ADD relation in Model Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,16 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'img_path', 'slug', 'stack'];
+    protected $fillable = ['title', 'description', 'img_path', 'slug', 'stack', 'type_id'];
 
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
     }
 }
