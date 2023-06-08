3ea198302ab2296e089c72c2d0ba4fe92daeb9a2
giuseppesciacca <sciaccagi@gmail.com>
2023-06-08 16:15:52 +0200

    ADD relation in model Project and Tecnology

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -21,5 +22,10 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function tecnologies(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\Tecnology');
     }
 }
