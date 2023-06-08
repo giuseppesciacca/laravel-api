c7196c49d4498812d25b58e472bcc59f05ab0059
giuseppesciacca <sciaccagi@gmail.com>
2023-06-08 16:18:52 +0200

    FIX name pivot table. Now in alphabetic order

--- database/migrations/2023_06_08_141821_create_project_tecnology_table.php
+++ database/migrations/2023_06_08_141821_create_project_tecnology_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tecnology_project', function (Blueprint $table) {
+        Schema::create('project_tecnology', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tecnology_project');
+        Schema::dropIfExists('project_tecnology');
     }
 };
